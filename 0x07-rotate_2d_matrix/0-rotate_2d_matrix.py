49628cd9236b4232039f737dd69b953e7084b716
EugineI <eugineindimuli@gmail.com>
2025-06-08 14:38:43 +0300

    reverse 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def rotate_2d_matrix(matrix):
+    """ rotate 2d matrix"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            for row in matrix:
+                row.reverse()
