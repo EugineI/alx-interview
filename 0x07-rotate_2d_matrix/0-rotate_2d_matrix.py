3a2f09ed543e183e2aaa5aaa5c21ad321112585c
EugineI <eugineindimuli@gmail.com>
2025-06-08 14:45:54 +0300

    test1

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,5 +8,5 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            for row in matrix:
-                row.reverse()
+    for row in matrix:
+        row.reverse()
