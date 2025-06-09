b085bc4af456fd82f61fb7fe79d66638823ba8f5
EugineI <eugineindimuli@gmail.com>
2025-06-09 09:25:47 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fewest number of coins"""
+
+
+def makeChange(coins, total):
+    """ reurn fewest no. of coins """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
