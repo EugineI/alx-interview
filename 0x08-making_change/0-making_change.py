a008ccb0c4664761905d65c1a05f5a2363bf208c
EugineI <eugineindimuli@gmail.com>
2025-06-10 23:40:57 +0300

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,15 +3,14 @@
 
 
 def makeChange(coins, total):
-    """ reurn fewest no. of coins """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+        for amount in range(coin, total + 1):
+            if dp[amount - coin] + 1 < dp[amount]:
+                dp[amount] = dp[amount - coin] + 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
